d3c3ad6f8adb74daaa007842b158bd94207dec35
Mayur Tekale <mayurtekale7@gmail.com>
2024-10-30 21:31:51 +0530

    creates all components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,25 @@
-import React from 'react'
+import React,{lazy} from "react";
+// lazy for code spliting
+import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
+//import Home from "./pages/Home";  insted of this we import dynmicaly like a following
 
+const Home = lazy(() => import('./pages/Home'));
+const Login = lazy(() => import("./pages/Login"));
+const Chat = lazy(() => import("./pages/Chat"));
+const Groups = lazy(() => import("./pages/Groups"));
 function App() {
   return (
-    <div>
-      
-    </div>
-  )
+    <BrowserRouter>
+     
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/login" element={<Login/>} />
+        <Route path="/chat/:chatId" element={<Chat/>} />
+        <Route path="/group" element={<Groups/>} />
+
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Heyyy</h1>
+    </div>
+  )
+}
+
+export default Home
+
+
