e6669cb70dfc5546df54358c6f1190b3ed51edf8
Mayur Tekale <mayurtekale7@gmail.com>
2024-11-02 22:20:12 +0530

    style avtar

--- client/src/components/Style/StyledComponents.jsx
+++ client/src/components/Style/StyledComponents.jsx
@@ -1,15 +1,17 @@
 import { styled } from "@mui/material";
 
-const VisuallyHiddenInput = styled('input')({
-    border: 0,
-    clip: 'rect(0 0 0 0)',
-    height: 1,
-    margin: -1,
-    overflow: 'hidden',
-    padding: 0,
-    position: 'absolute',
-    whiteSpace: 'normal',
-    width: 1,
+// we styled that input this is add image when click on avtar
+
+const VisuallyHiddenInput = styled("input")({
+  border: 0,
+  clip: "rect(0 0 0 0)",
+  height: 1,
+  margin: -1,
+  overflow: "hidden",
+  padding: 0,
+  position: "absolute",
+  whiteSpace: "normal",
+  width: 1,
 });
 
 // Default export
