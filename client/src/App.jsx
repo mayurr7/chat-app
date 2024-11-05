9e1be491bd6d0db5c8085836f254bd9493ebaa94
Mayur Tekale <mayurtekale7@gmail.com>
2024-11-05 22:35:18 +0530

    app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,22 +1,38 @@
 import React,{lazy} from "react";
 // lazy for code spliting
 import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
+import ProtectRoute from "./components/auth/ProtectRoute";
 //import Home from "./pages/Home";  insted of this we import dynmicaly like a following
 
 const Home = lazy(() => import('./pages/Home'));
 const Login = lazy(() => import("./pages/Login"));
 const Chat = lazy(() => import("./pages/Chat"));
 const Groups = lazy(() => import("./pages/Groups"));
+const PageNotFound = lazy(() => import("./pages/PageNotFound"))
+let user = true;
+
 function App() {
   return (
     <BrowserRouter>
      
       <Routes>
-        <Route path="/" element={<Home/>} />
-        <Route path="/login" element={<Login/>} />
-        <Route path="/chat/:chatId" element={<Chat/>} />
-        <Route path="/group" element={<Groups/>} />
+        <Route  element={
+        <ProtectRoute user={user} />}>
+          <Route path="/" element={<Home/>} />
+             <Route path="/chat/:chatId" element={<Chat/>} />
+             <Route path="/group" element={<Groups/>} />
+        </Route>       
+        <Route path="/login" 
+        element={
+         <ProtectRoute user={!user} redirect="/">
+             <Login/>
+         </ProtectRoute>
+        }
+        />
 
+        {/* if error in page then this route */}
+        <Route path="*" element ={ <PageNotFound/>} />
+         
       </Routes>
     </BrowserRouter>
   );
