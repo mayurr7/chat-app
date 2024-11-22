5b94bba02dfefbfa6dbc6ef7583ab8f71e1933a3
Mayur Tekale <mayurtekale7@gmail.com>
2024-11-23 00:04:07 +0530

    chatlist and chatItem

---
+++ client/src/components/Shared/ChatItem.jsx
@@ -0,0 +1,62 @@
+import React, { memo } from 'react'
+import { Link } from '../Style/StyledComponents';
+import { Box, Stack, Typography } from '@mui/material';
+
+const ChatItem = ({
+    avatae = [],
+    name,
+    _id,
+    groupChat = false,
+    sameSender,
+    isOnline,
+    newMessageAlert,
+    index = 0,
+    handleDeleteChatOpen,
+}) => {
+  return <Link to={`/chat/${_id}`}  
+  onContextMenu={(e) => handleDeleteChatOpen(e, _id, groupChat)}
+  >
+    <div style={{
+        display: "flex",
+        gap: "1rem",
+        alignItems: "center",
+        padding: "1rem",
+        backgroundColor: sameSender ? "black" : "unset",
+        color: sameSender ? "white" : "unset",
+        position: "relative",
+    }}
+    >
+
+        {/* Avatar card */}
+        <Stack>
+            <Typography>{name}</Typography>
+            {
+                newMessageAlert && (
+                    <Typography>{newMessageAlert.count} New Messagre</Typography>
+                )
+            }
+        </Stack>
+
+        {
+            isOnline &&  (
+                <Box 
+                sx={{
+                    width: "10px",
+                    height: "10px",
+                    borderRadius: "50%",
+                    backgroundColor: "green",
+                    position: "absolute",
+                    top: "50%",
+                    right: "1rem",
+                    transform: "translateY(-50%)",
+                }}
+                />
+            )
+        }
+
+    </div>
+  </Link>  
+  
+}
+
+export default memo(ChatItem);
