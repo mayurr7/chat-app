d558b659aedbb8d2b323a17b6d422c4a37878110
Mayur Tekale <mayurtekale7@gmail.com>
2024-12-06 18:46:25 +0530

    notification

--- client/src/components/specific/Notifications.jsx
+++ client/src/components/specific/Notifications.jsx
@@ -1,11 +1,36 @@
-import React from 'react'
+import React from 'react';
+import { Dialog, DialogTitle, InputAdornment, List, ListItem, ListItemText, Stack, TextField, Typography } from '@mui/material';
+ import { sampleNotifications } from '../../Constants/sampleData';
 
 const Notifications = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  return (<Dialog open>
+    <Stack
+      p={{
+        xs: "1rem", sm:"2rem"
+      }} maxWidth={"25rem"}
+    >
+      <DialogTitle>Notifications</DialogTitle>
+
+      {
+        sampleNotifications.length > 0 ? (
+            sampleNotifications.map(({sender, _id}) =>(
+              <NotificationItem 
+              sender={sender}
+              _id={_id}
+              handler={friendRequestHandler}
+              key={_id}
+              />
+            ))
+        ): (<Typography textAlign={"center"}>0 Notifications</Typography>
+     )}
+    </Stack>
+  </Dialog>
+  );
+};
+
+const NotificationItem = ({ sender, _id, handler}) => {
+  return <></>;
+};
+
 
 export default Notifications
