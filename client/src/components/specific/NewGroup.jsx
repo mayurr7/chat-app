f5e2c83686e906b98b02a4ad796e09e0daa93b3a
Mayur Tekale <mayurtekale7@gmail.com>
2024-12-06 20:36:36 +0530

    new Group

--- client/src/components/specific/NewGroup.jsx
+++ client/src/components/specific/NewGroup.jsx
@@ -1,11 +1,71 @@
-import React from 'react'
+import React, { useState } from "react";
+import {
+  Avatar,
+  Button,
+  Dialog,
+  DialogTitle,
+  ListItem,
+  Stack,
+  TextField,
+  Typography,
+} from "@mui/material";
+import { sampleUsers } from "../../Constants/sampleData";
+import UserItem from "../Shared/UserItem";
+import { useInputValidation } from "6pp";
 
 const NewGroup = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const groupName = useInputValidation("");
 
-export default NewGroup
+  const [members, setMembers] = useState(sampleUsers);
+  const [selectedMembers, setSelectedMembers] = useState([]);
+
+  const selectMemberHandler = (id) => {
+    setSelectedMembers((prev) => 
+    (prev.includes(id) 
+    ? prev.filter((currEle) => currEle !== id)    
+    : [...prev, id]));
+  };
+
+  const submitHandler = () => {};
+  return (
+    <Dialog open>
+      <Stack
+        p={{
+          xs: "1rem",
+          sm: "3rem",
+        }}
+        width={"25rem"}
+        spacing={"2rem"}
+      >
+        <DialogTitle textAlign={"center"} variant="h4">
+          New Group
+        </DialogTitle>
+
+        <TextField
+          label="Group Name"
+          value={groupName.value}
+          onChange={groupName.changeHandler}
+        />
+
+        <Typography variant="body1">Members</Typography>
+
+        <Stack>
+          {members.map((i) => (
+            <UserItem user={i} key={i._id} handler={selectMemberHandler} />
+          ))}
+        </Stack>
+
+        <Stack direction={"row"} justifyContent={"space-evenly"}>
+          <Button variant="text" color="error" size="large">
+            Cancel
+          </Button>
+          <Button variant="contained" size="large" onClick={submitHandler}>
+            Create
+          </Button>
+        </Stack>
+      </Stack>
+    </Dialog>
+  );
+};
+
+export default NewGroup;
