f03356b4fd5312da1c512288b5773f56a7f9cb5d
Mayur Tekale <mayurtekale7@gmail.com>
2024-12-06 20:36:57 +0530

    main file

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -8,7 +8,10 @@ createRoot(document.getElementById("root")).render(
   <StrictMode>
     <HelmetProvider>
       <CssBaseline />
+      <div onContextMenu={(e) => e.preventDefault()}>
       <App />
+      </div>
+     
     </HelmetProvider>
   </StrictMode>
 );
