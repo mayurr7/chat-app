c4591a1797d87ba2e79780451a9b062c38a2e798
Mayur Tekale <mayurtekale7@gmail.com>
2024-12-08 20:38:33 +0530

    id not added for chat

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,42 +1,40 @@
-import React,{lazy, Suspense} from "react";
+import React, { lazy, Suspense } from "react";
 // lazy for code spliting
 import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
 import ProtectRoute from "./components/auth/ProtectRoute";
 import Loaders from "./components/layout/Loaders";
 //import Home from "./pages/Home";  insted of this we import dynmicaly like a following
 
-const Home = lazy(() => import('./pages/Home'));
+const Home = lazy(() => import("./pages/Home"));
 const Login = lazy(() => import("./pages/Login"));
 const Chat = lazy(() => import("./pages/Chat"));
 const Groups = lazy(() => import("./pages/Groups"));
-const PageNotFound = lazy(() => import("./pages/PageNotFound"))
+const PageNotFound = lazy(() => import("./pages/PageNotFound"));
 let user = true;
 
 function App() {
   return (
     <BrowserRouter>
-      <Suspense fallback={<Loaders/>}>
-      <Routes>
-        <Route  element={
-        <ProtectRoute user={user} />}>
-          <Route path="/" element={<Home/>} />
-             <Route path="/chat" element={<Chat/>} />
-             <Route path="/group" element={<Groups/>} />
-        </Route>       
-        <Route path="/login" 
-        element={
-         <ProtectRoute user={!user} redirect="/">
-             <Login/>
-         </ProtectRoute>
-        }
-        />
+      <Suspense fallback={<Loaders />}>
+        <Routes>
+          <Route element={<ProtectRoute user={user} />}>
+            <Route path="/" element={<Home />} />
+            <Route path="/chat/:id" element={<Chat />} />
+            <Route path="/group" element={<Groups />} />
+          </Route>
+          <Route
+            path="/login"
+            element={
+              <ProtectRoute user={!user} redirect="/">
+                <Login />
+              </ProtectRoute>
+            }
+          />
 
-        {/* if error in page then this route */}
-        <Route path="*" element ={ <PageNotFound/>} />
-         
-      </Routes>
+          {/* if error in page then this route */}
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
       </Suspense>
-     
     </BrowserRouter>
   );
 }
