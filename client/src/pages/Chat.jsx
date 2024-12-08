2681102300565cb51dc44598da8ce3e19144d365
Mayur Tekale <mayurtekale7@gmail.com>
2024-12-08 20:59:10 +0530

    chat box done

--- client/src/pages/Chat.jsx
+++ client/src/pages/Chat.jsx
@@ -1,11 +1,53 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import AppLayout from '../components/layout/AppLayout';
+import { IconButton, Stack } from '@mui/material';
+import { greayColor } from '../Constants/color';
+import ChatItem from '../components/Shared/ChatItem';
+import { AttachFile as AttachFileIcon, Send as SendIcon} from '@mui/icons-material';
+import { InputBox } from '../components/Style/StyledComponents';
 
 const Chat = () => {
+  const containerRef = useRef(null);
+
   return (
-    <div>
-      <h1>letss talk</h1>
-    </div>
+    <>
+      <Stack
+        ref={containerRef}
+        boxSizing={"border-box"}
+        padding={"1rem"}
+        spacing={"1rem"}
+        bgcolor={greayColor}
+        height={"80vh"}
+        sx={{
+          overflowX: "hidden",
+          overflowY: "auto",
+        }}
+      >
+          {/* Message Rnder */}
+      </Stack>
+
+      <form 
+        style={{
+          height: "10%",
+        }}
+      >
+        <Stack direction={"row"} height={"100%"}
+        padding={"1rem"}
+        alignItems={"center"}
+        position={"relative"}
+        >
+          <IconButton>
+            <AttachFileIcon />
+          </IconButton>
+
+          <InputBox placeholder='Type Message...'/>
+
+          <IconButton>
+            <SendIcon />
+          </IconButton>
+        </Stack>
+      </form>
+    </>
   )
 }
 
