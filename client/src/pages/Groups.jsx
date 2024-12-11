efdf4a1e8e1f8c9cf9bf5bdba82d4494aab97b09
Mayur Tekale <mayurtekale7@gmail.com>
2024-12-11 14:54:35 +0530

    groups ui

--- client/src/pages/Groups.jsx
+++ client/src/pages/Groups.jsx
@@ -1,11 +1,39 @@
-import React from 'react'
+import React from "react";
+import { Grid } from "@mui/material";
 
 const Groups = () => {
   return (
-    <div>
-      <h1>create group</h1>
-    </div>
-  )
-}
+    <Grid container height={"100vh"}>
+      <Grid
+        item
+        sx={{
+          display: {
+            xs: "none",
+            sm: "block",
+          },
+        }}
+        sm={4}
+        bgcolor={"bisque"}
+      >
+        Group List
+      </Grid>
 
-export default Groups
+      <Grid
+        item
+        xs={12}
+        sm={8}
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          position: "relative",
+          padding: "1rem 3rem",
+        }}
+      >
+        tatya group
+      </Grid>
+    </Grid>
+  );
+};
+
+export default Groups;
