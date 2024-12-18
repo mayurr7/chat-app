46dc8763f378735a737cf685694d4ad91c41cc3c
Mayur Tekale <mayurtekale7@gmail.com>
2024-12-18 20:47:10 +0530

    dashboard for admin

---
+++ client/src/pages/admin/DashBoard.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import AdminLayout from '../../components/layout/AdminLayout'
+
+const DashBoard = () => {
+  return (
+   <>
+        <AdminLayout>
+            <div>DashBoard</div>
+        </AdminLayout>
+   </>
+  )
+}
+
+export default DashBoard
