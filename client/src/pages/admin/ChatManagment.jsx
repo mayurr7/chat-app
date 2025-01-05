c3880ba24e03d62cae9eba6c0922b5d001494bed
Mayur Tekale <mayurtekale7@gmail.com>
2025-01-05 17:56:28 +0530

    managmwnt component created

---
+++ client/src/pages/admin/ChatManagment.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import AdminLayout from '../../components/layout/AdminLayout'
+
+const ChatManagment = () => {
+  return (
+    <AdminLayout>
+        <div>ChatManagment</div>
+    </AdminLayout>
+  )
+}
+
+export default ChatManagment
---
+++ client/src/pages/admin/UserManagment.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import AdminLayout from '../../components/layout/AdminLayout';
+
+const UserManagment = () => {
+  return (
+     <AdminLayout>
+        <div>UserMAnagment</div>
+    </AdminLayout>
+  )
+}
+
+export default UserManagment
