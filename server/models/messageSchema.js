a54ea775da035310283fafeb00837cbef2869456
Mayur Tekale <mayurtekale7@gmail.com>
2025-03-07 16:11:58 +0530

    message schema created

---
+++ server/models/messageSchema.js
@@ -0,0 +1,38 @@
+import mongoose,{ Schema, Types, model } from "mongoose";
+
+
+const messageSchema = new Schema(
+  {
+    content: String,
+
+    attachment: [
+      {
+        public_id: {
+          type: String,
+          require: true,
+        },
+        url: {
+          type: String,
+          require: true,
+        },
+      },
+    ],
+
+    sender: {
+      type: Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+
+    creator: {
+      type: Types.ObjectId,
+      ref: "Chat",
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const Message = mongoose.models.Message || model("Message", messageSchema);
