1e2c149f3cf565324f43fb3b3ced4cc18a68bb26
Mayur Tekale <mayurtekale7@gmail.com>
2025-03-07 16:12:41 +0530

    message schema created

---
+++ server/models/requestSchema.js
@@ -0,0 +1,31 @@
+import mongoose,{ Schema, Types, model } from "mongoose";
+
+
+const Schema = new Schema(
+  {
+    content: String,
+
+    status: {
+      type: String,
+      default: "pending",
+      enum: ["pending", "accepted", "rejected"],
+    },
+
+    sender: {
+      type: Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+
+    recevier: {
+      type: Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const Request = mongoose.models.Request || ("Request", Schema);
