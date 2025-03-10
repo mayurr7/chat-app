66fbf86064e3b485b4272e72806110f8c57338cc
Mayur Tekale <mayurtekale7@gmail.com>
2025-03-10 17:57:47 +0530

    middeleware added

--- server/app.js
+++ server/app.js
@@ -1,10 +1,28 @@
 import express from "express";
 import userRoute from './routes/user.js';
+import { connectDb } from "./utils/features.js";
+import dotenv from "dotenv";
+
+dotenv.config({
+    path: "./.env",
+});
+
+const mongoURI = process.env.MONGO_URI;
+const PORT = process.env.PORT || 5000;
+
+connectDb(mongoURI);
 
 const app  = express();
 
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    console.log("home page");
+    
+})
+
 app.use('/user', userRoute);
 
-app.listen(3000, () => {
-    console.log("server is running on port 3000");
+app.listen(PORT, () => {
+    console.log(`server is running on port ${PORT}`);
 });
