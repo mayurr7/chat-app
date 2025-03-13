60af1087c7ecd020d973f2404e48ee694434689c
Mayur Tekale <mayurtekale7@gmail.com>
2025-03-13 16:02:17 +0530

    update

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,6 +1,48 @@
-const login = (req, res) =>  {
-    console.log("get router callled");
-    
+import { compare } from "bcrypt";
+import { User } from "../models/userSchema.js";
+import { sendToken } from "../utils/features.js";
+
+const newUser = async (req, res) => {
+  const { name, username, password, bio } = req.body;
+
+  const avatar = {
+    public_id: "dfdfds",
+    url: "sdbhsd",
+  };
+
+  const user = await User.create({
+    name,
+    username,
+    password,
+    avatar,
+    bio,
+  });
+
+  sendToken(res, user, 201, "User created");
 };
 
-export {login};
+const login = async (req, res, next) => {
+  try {
+    const { username, password } = req.body;
+
+  const user = await User.findOne({ username }).select("+password");
+
+  if (!user) return next(new Error("Invalid Username"));
+
+  const isMatch = await compare(password, user.password);
+
+  if (!isMatch)  return next(new Error("Invalid Password"));
+
+  sendToken(res, user, 200, `Welcome back ${user.name}`);
+
+  } catch (error) {
+    next(error);
+  }
+};
+
+
+
+const getMyProfile = (req, res) => {
+
+};
+export { login, newUser, getMyProfile };
