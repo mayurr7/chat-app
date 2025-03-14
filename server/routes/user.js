6cdb2526f0aed666aa8570554f2f01b471c425df
Mayur Tekale <mayurtekale7@gmail.com>
2025-03-14 19:49:57 +0530

    update

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,11 +1,15 @@
 import express from 'express';
-import { login, newUser} from '../controllers/user.js';
+import { getMyProfile, login, newUser} from '../controllers/user.js';
 import { singleAvatar } from "../middlewares/multer.js"
+import { isAuthenticated } from '../middlewares/auth.js';
 
 const app = express();
 
 app.post('/newuser', singleAvatar, newUser)
 
 app.post('/login', login);
+
+
+app.get('/me', isAuthenticated, getMyProfile)
 
 export default app;
