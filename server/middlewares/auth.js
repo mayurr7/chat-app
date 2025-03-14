82eeff3a9b2e90a8c068307a5846d8e97957fb6a
Mayur Tekale <mayurtekale7@gmail.com>
2025-03-14 19:49:17 +0530

    user authentication

---
+++ server/middlewares/auth.js
@@ -0,0 +1,21 @@
+import  jwt  from "jsonwebtoken";
+import { ErrorHandler } from "../utils/utility.js";
+
+const isAuthenticated = async(req, res, next) => {
+    try {
+        const token = req.cookies["chatt-token"];
+
+        if(!token) return new ErrorHandler("Please login to access this route", 401);
+
+        const decodeToken = jwt.verify(token, process.env.JWT_SECRET);
+
+        req.user = decodeToken._id;
+        
+        next();
+        
+    } catch (error) {
+        next(error);
+    }
+}
+
+export { isAuthenticated };
