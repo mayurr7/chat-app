f660fcfcb2ff72c5910048ce5b7a3d60181ff5cc
Mayur Tekale <mayurtekale7@gmail.com>
2025-03-18 17:18:07 +0530

    user sedder created

---
+++ server/seeders/userSeeders.js
@@ -0,0 +1,33 @@
+import { User } from "../models/userSchema.js";
+import { faker } from "@faker-js/faker";
+
+const createUser = async (numUser) => {
+  try {
+    const userPromise = [];
+
+    for (let i = 0; i < numUser; i++) {
+      const tempUser = User.create({
+        name: faker.person.fullName(),
+        username: faker.internet.userName(),
+        bio: faker.lorem.sentence(10),
+        password: "123456789",
+        avatar: {
+          url: faker.image.avatar(),
+          public_id: faker.system.fileName(),
+        },
+      });
+
+      userPromise.push(tempUser);
+    }
+
+    await Promise.all(userPromise);
+
+    // console.log(`${numUser} users created successfully!`);
+    process.exit(0);  
+  } catch (error) {
+    console.error("Error creating users:", error);
+    process.exit(1);
+  }
+};
+
+export { createUser };
