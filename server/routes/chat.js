a6e5b3e06f335e9060873e5a8998199f7277e995
Mayur Tekale <mayurtekale7@gmail.com>
2025-03-19 18:01:03 +0530

    update

--- server/routes/chat.js
+++ server/routes/chat.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import { isAuthenticated } from '../middlewares/auth.js';
+import { getMyChat, getMyGroups, newGroupChat } from '../controllers/chat.js';
 
 const app = express();
 
@@ -8,6 +9,10 @@ const app = express();
 
 
 app.use(isAuthenticated);
+
+app.post('/new', newGroupChat);
+app.get('/my', getMyChat);
+app.get('/my/group', getMyGroups);
 
 
 
