54021a9e39e9339044a79689a2b6a582091f490c
Mayur Tekale <mayurtekale7@gmail.com>
2025-03-26 16:20:46 +0530

    update

--- server/routes/chat.js
+++ server/routes/chat.js
@@ -1,9 +1,8 @@
 import express from 'express';
 import { isAuthenticated } from '../middlewares/auth.js';
-import { addMembers, getMyChat, getMyGroups, newGroupChat } from '../controllers/chat.js';
+import { addMembers, getMyChat, getMyGroups, leaveGroup, newGroupChat, removeMember } from '../controllers/chat.js';
 
 const app = express();
-
 
 
 
@@ -14,6 +13,9 @@ app.post('/new', newGroupChat);
 app.get('/my', getMyChat);
 app.get('/my/group', getMyGroups);
 app.put('/addmembers', addMembers);
+app.put('/removemember', removeMember);
+app.delete('/leave/:id', leaveGroup);
+
 
 
 
