b5dfec46105e532bf22099bb2dbcf49b67633767
Mayur Tekale <mayurtekale7@gmail.com>
2025-05-02 18:21:35 +0530

    update

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,8 +1,8 @@
 import express from 'express';
-import { getMyProfile, login, logOut, newUser, searchUser, sendFriendRequest} from '../controllers/user.js';
+import { acceptFriendRequest, getAllNotification, getMyFriends, getMyProfile, login, logOut, newUser, searchUser, sendFriendRequest} from '../controllers/user.js';
 import { singleAvatar } from "../middlewares/multer.js"
 import { isAuthenticated } from '../middlewares/auth.js';
-import { registerValidator, validateHandler, loginValidator, sendRequestValidator } from '../lib/validator.js';
+import { registerValidator, validateHandler, loginValidator, sendRequestValidator, acceptRequestValidator } from '../lib/validator.js';
 
 
 const app = express();
@@ -15,8 +15,18 @@ app.post('/login',loginValidator(),validateHandler, login);
 app.use(isAuthenticated);
 
 app.get('/me', getMyProfile);
+
 app.post('/logout', logOut);
+
 app.post('/searchuser', searchUser);
-app.put('/sendrequest', sendRequestValidator(), validateHandler, sendFriendRequest)
+
+app.put('/sendrequest', sendRequestValidator(), validateHandler, sendFriendRequest);
+
+app.put('/acceptrequest', acceptRequestValidator(), validateHandler, acceptFriendRequest);
+
+
+app.get('/notification', getAllNotification);
+
+app.get('/myfriends', getMyFriends)
 
 export default app;
