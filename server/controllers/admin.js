6d63471cf15b60e9422efb4dfea243d1ca29622a
Mayur Tekale <mayurtekale7@gmail.com>
2025-05-03 18:40:37 +0530

    all user  and allchats for admin created

---
+++ server/controllers/admin.js
@@ -0,0 +1,81 @@
+import { User } from "../models/userSchema.js";
+import { Chat } from "../models/chatSchema.js";
+import { Message } from "../models/messageSchema.js";
+
+const allUsers = async (req, res, next) => {
+  try {
+    const users = await User.find({});
+
+    const transformedUsers = await Promise.all(
+      users.map(async ({ username, name, avatar, _id }) => {
+        const [groups, friends] = await Promise.all([
+          Chat.countDocuments({ groupChat: true, members: _id }),
+          Chat.countDocuments({ groupChat: false, members: _id }),
+        ]);
+
+        return {
+          name,
+          username,
+          avatar: avatar.url,
+          _id,
+          groups,
+          friends,
+        };
+      })
+    );
+
+    return res.status(200).json({
+      status: "sucess",
+      users: transformedUsers,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+const allChats = async (req, res, next) => {
+  try {
+    const chats = await Chat.find({})
+      .populate("members", "name avatar")
+      .populate("creator", "name avatar");
+
+    const transformedChats = await Promise.all(
+      chats.map(async ({ members, _id, groupChat, name, creator }) => {
+
+        const totalMessages = await Message.countDocuments({ chat: _id});
+
+        return {
+          _id,
+          groupChat,
+          name,
+          avatar: members.slice(0, 3).map((member) => member.avatar.url),
+          members: members.map(({ _id, name, avatar }) => (
+            {
+                
+                  _id,
+                  name,
+                  avatar: avatar.url,
+               
+              }
+          )),
+          creator: {
+            name: creator?.name || "none",
+            avatar: creator?.avatar.url || "",
+          },
+          totalMembers: members.length,
+          totalMessages,
+        };
+      })
+    );
+
+    return res.status(200).json({
+      status: "sucess",
+      chats: transformedChats,
+    });
+
+  } catch (error) {
+    next(error);
+  }
+};
+
+export { allUsers, allChats };
