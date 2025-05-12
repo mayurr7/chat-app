77d8dc50ecefedaad0a62b6e7edf4751368ddaf4
Mayur Tekale <mayurtekale7@gmail.com>
2025-05-12 18:43:00 +0530

    update route

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -1,10 +1,14 @@
 import express from "express";
-import { allChats, allUsers } from "../controllers/admin.js";
+import { allChats, allMessages, allUsers, getDashBoardStats } from "../controllers/admin.js";
 
 const app = express.Router();
 
 app.get('/users', allUsers);
 
-app.get('/chats', allChats)
+app.get('/chats', allChats);
+
+app.get('/messages', allMessages);
+
+app.get('/stats', getDashBoardStats);
 
 export default app;
