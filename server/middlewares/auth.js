b7582f85baf61162f308044d96138f37c5f527f4
Mayur Tekale <mayurtekale7@gmail.com>
2025-05-16 12:51:21 +0530

    admmin authentication aadded

--- server/middlewares/auth.js
+++ server/middlewares/auth.js
@@ -1,5 +1,6 @@
 import jwt from "jsonwebtoken";
 import { ErrorHandler } from "../utils/utility.js";
+import { adminSecretKey } from "../app.js";
 
 const isAuthenticated = async (req, res, next) => {
   try {
@@ -17,4 +18,23 @@ const isAuthenticated = async (req, res, next) => {
   }
 };
 
-export { isAuthenticated };
+
+const adminOnly = async (req, res, next) => {
+  try {
+    const token = req.cookies["chat-admin-token"];    
+
+    if (!token) return next(new ErrorHandler("Only admin can access this route", 401));
+
+    const secretKey = jwt.verify(token, process.env.JWT_SECRET);
+
+    const isMatched = secretKey === adminSecretKey;
+
+    if (!isMatched) return next(new ErrorHandler("Only admin can access this route", 401));
+
+    next();
+  } catch (error) {
+    next(error);
+  }
+};
+
+export { isAuthenticated, adminOnly };
